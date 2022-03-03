3f87ad9b058eb9caf4aac92659deff11d51ba994
Elis Odenhage <teknikgillaren@gmail.com>
2022-03-03 20:28:50 +0100

    Adding some code

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const Discord = require('discord.js')
-const bot = new Discord.Client()
+const bot = new Discord.Client({intents: [Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.GUILDS]})
+require('dotenv').config()
+bot.on('ready', () => {
+    console.log("The bot is online!")
+})
+
+
+bot.login(process.env.BOT_TOKEN)

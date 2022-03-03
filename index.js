d009103cb1900b1fcd1b38894f67b360d545af60
Elis Odenhage <teknikgillaren@gmail.com>
2022-03-03 20:37:13 +0100

    some more code

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const bot = new Discord.Client({intents: [Discord.Intents.FLAGS.GUILD_MEMBERS, D
 require('dotenv').config()
 bot.on('ready', () => {
     console.log("The bot is online!")
+
+    let commands = bot.application.commands
+
+    commands.create({
+        name: "hello",
+        description: "replys somthin'"
+    })
 })
+
+bot.on('interacitonCreate',)
 
 
 bot.login(process.env.BOT_TOKEN)
